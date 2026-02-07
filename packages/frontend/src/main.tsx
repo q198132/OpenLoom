c8c23295be129c293b33f2ee44d8d26f743c03ca
q198132 <1981329791@qq.com>
2026-02-08 04:58:41 +0800

    update(packages/frontend/src/main.tsx): main.tsx

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { loader } from '@monaco-editor/react';
+import * as monaco from 'monaco-editor';
 import App from './App';
 import './app.css';
+
+// 使用本地打包的 Monaco，不从 CDN 加载
+loader.config({ monaco });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
